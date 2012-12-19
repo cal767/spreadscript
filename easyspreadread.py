12fa766dcd54e28caf688b7e868ad7e86f3e649d
Calum Leslie <cjaleslie@gmail.com>
2012-12-19 12:13:30 +0000

    Function naming conventions corrected

--- easyspreadread.py
+++ easyspreadread.py
@@ -11,7 +11,7 @@
 
 import xlrd
 
-def readSheet(filetoread):
+def read_sheet(filetoread):
     ''' Takes a .xls file to read and outputs cell data in the format
     of a list of lists where the innermost lists represent a row of
     information, for a 2x2 spreadsheet this would be in the format:
@@ -40,8 +40,9 @@ def readSheet(filetoread):
 
     return completedList
 
-def writeToTxt(text_file, input_list):
-    ''' Writes a list to a text file
+def write_info(text_file, input_list):
+    ''' Writes information taken from spreadshet to
+     a file in the format of a list of lists
     '''
     f = open(text_file, 'w')
     for item in input_list:
@@ -51,7 +52,7 @@ def writeToTxt(text_file, input_list):
 def main():
     sheet = raw_input("Please enter the name of the spreadsheet you want to take the data from (format: testspreadsheet.xls)")
     writetxt = raw_input("Please enter the name of the text file you want to write the information to (format: test.txt)")
-    writeToTxt(writetxt, readSheet(sheet))
+    write_info(writetxt, read_sheet(sheet))
 
 if __name__ == '__main__':
     main()
